e684bbe74b0c31738eb0dc4b3c48a2cb2286efe2
Jose Manuel <jose.wt@gmail.com>
2021-06-10 19:10:10 -0400

    se configura titulo 2 y color fondo 2

--- WebContent/devolucion/app/components/personalizar-sitio/personalizar-sitio.js
+++ WebContent/devolucion/app/components/personalizar-sitio/personalizar-sitio.js
@@ -9,29 +9,47 @@ angular.module('personalizarSitioModule')
                 var vm = this;
 
                 vm.editarTitulo1 = false;
-                vm.editarSubtitulo1 = false;
                 vm.editarTitulo2 = false;
+                vm.editarTitulo3 = false;
 
-                vm.colorPickerOptions = {
+                vm.colorPickerOptions1 = {
                     format: 'hex',
                     id: 'colorPickerTitulo1'
                 }
 
-                vm.config = {
-                    textoTitulo1: "Devolución Web",
-                    colorTitulo1: "858796"
+                vm.colorPickerOptions2 = {
+                    format: 'hex',
+                    id: 'colorPickerTitulo2'
                 }
 
-                vm.styleTitulo1 = {
-                    color: "#" + vm.config.colorTitulo1
+                vm.colorPickerOptionsColorFondo2 = {
+                    format: 'hex',
+                    id: 'colorPickerColorFondo2'
+                }
+
+                vm.config = {
+                    textoTitulo1: "Devolución Web",
+                    colorTitulo1: "858796",
+                    textoTitulo2: "¿Necesitas realizar una devolución? Completa el siguiente formulario:",
+                    colorTitulo2: "39c0e0",
+                    colorFondo2: "e0f1f7"
+                }
+
+                vm.style = {
+                    titulo1: {
+                        color: "#" + vm.config.colorTitulo1
+                    },
+                    titulo2: {
+                        color: "#" + vm.config.colorTitulo2
+                    },
+                    colorFondo2: {
+                        background: "#" + vm.config.colorFondo2
+                    }
                 }
 
                 vm.temp = {
-                    textoTitulo1: "Devolución Web"
-                }
-
-                vm.editarColorTitulo1 = function () {
-                    vm.titulo1ColorPicker.open();
+                    textoTitulo1: "Devolución Web",
+                    textoTitulo2: "¿Necesitas realizar una devolución? Completa el siguiente formulario:"
                 }
 
                 vm.guardarTitulo1 = function () {
@@ -39,10 +57,29 @@ angular.module('personalizarSitioModule')
                     vm.editarTitulo1 = false
                 }
 
+                vm.guardarTitulo2 = function () {
+                    vm.config.textoTitulo2 = angular.copy(vm.temp.textoTitulo2);
+                    vm.editarTitulo2 = false
+                }
+
                 vm.eventAPIColorTitulo1 = {
                     onChange: function (api, color, $event) {
                         vm.config.colorTitulo1 = color
-                        vm.styleTitulo1.color = "#" + color
+                        vm.style.titulo1.color = "#" + color
+                    }
+                }
+
+                vm.eventAPIColorTitulo2 = {
+                    onChange: function (api, color, $event) {
+                        vm.config.colorTitulo2 = color
+                        vm.style.titulo2.color = "#" + color
+                    }
+                }
+
+                vm.eventAPIColorColorFondo2 = {
+                    onChange: function (api, color, $event) {
+                        vm.config.colorFondo2 = color
+                        vm.style.colorFondo2.background = "#" + color
                     }
                 }
 
